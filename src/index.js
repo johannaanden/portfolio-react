6d96a189a2765659193be6f8ad587e14e09078ca
johannaanden <johannaanden@hotmail.com>
2019-01-07 15:10:09 +0100

    Imports Header and Footer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import Hello from "./Hello"
+import Header from "./Header"
+import Footer from "./Footer"
 
 const App = () => {
     return <Hello />
