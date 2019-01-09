15f6c3fcecf919d29f47e5a83fa7c55b4563c753
johannaanden <johannaanden@hotmail.com>
2019-01-09 13:37:10 +0100

    Installs ReactPlayer and adds video [W.I.P]

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,8 +1,16 @@
 import React from "react"
+import ReactPlayer from 'react-player'
 
 const Hello = () => {
     return (
-        <p className="content-text">Hello World</p>
+        <div>
+            <ReactPlayer
+                url='./src/images/bg_pink_1.mp4' 
+                playing 
+                loop 
+                width='100%'
+                height='100%'/>
+        </div>
     )
 }
 
