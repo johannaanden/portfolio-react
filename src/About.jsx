4f9d064b1d5d2383726dbbb58ff16f48fa9699af
johannaanden <johannaanden@hotmail.com>
2019-01-09 12:10:39 +0100

    Adds image to 'About Me' page

--- src/About.jsx
+++ src/About.jsx
@@ -1,21 +1,21 @@
 import React from "react"
-import { UndrawDesignerLife } from 'react-undraw-illustrations';
+import LazyLoad from 'react-lazyload';
 
 const About = () => {
     return (
-        <div className="content-wrapper">
+        <div className="flex items-center justify-center">
             <div className="flex flex-wrap mb-4">
-                <div className="w-full lg:w-1/4 md:w-1/2">
-                    <UndrawDesignerLife primaryColor='#12283a' height='200px' />
-                </div>
+                <LazyLoad height={100}>
+                    <img className="border border-white rounded-full h-48 w-48" src="./src/images/johanna.JPG" />
+                </LazyLoad>
 
                 <div className="w-full lg:w-3/4 md:w-1/2">
                     <h1 className="content-text">About Me</h1>
                     <p className="content-text">Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
                 </div>
-            
+
             </div>
-        </div> 
+        </div>
     )
 }
 
