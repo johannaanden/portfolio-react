c0a987bc9d98b33bf173459f4fc9f9424cd1c3a9
johannaanden <johannaanden@hotmail.com>
2019-01-09 12:19:16 +0100

    Updates infomation content on 'About me' page

--- src/About.jsx
+++ src/About.jsx
@@ -3,15 +3,18 @@ import LazyLoad from 'react-lazyload';
 
 const About = () => {
     return (
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center mt-20">
             <div className="flex flex-wrap mb-4">
                 <LazyLoad height={100}>
                     <img className="border border-white rounded-full h-48 w-48" src="./src/images/johanna.JPG" />
                 </LazyLoad>
 
-                <div className="w-full lg:w-3/4 md:w-1/2">
-                    <h1 className="content-text">About Me</h1>
-                    <p className="content-text">Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+                <div className="w-full lg:w-3/4 md:w-1/2 ml-8">
+                    <h1 className="content-text">Hello!</h1>
+                    <br/>
+                    <p className="content-text">My name is Johanna Andén, I am a student at Craft Academy where I study the course Full stack web developer.</p>
+                    <br/>
+                    <p className="content-text">When I'm not studying, I work at the TV and broadband provider Sappa in Gothenburg.</p>
                 </div>
 
             </div>
